608625ed24bedd1f38ce5178c92acd2aa2b761b8
Isaac López <izaak00@hotmail.com>
2017-04-29 16:44:08 -0500

    Templates

--- blog/Posts/views.py
+++ blog/Posts/views.py
@@ -28,7 +28,7 @@ def post_list(request):
 	context = {
 		'queryset':queryset
 	}
-	return render(request, "index.html",context)
+	return render(request, "post_list.html",context)
 
 def post_detail(request, id=None):
 	instance = get_object_or_404(Post, id=id)
